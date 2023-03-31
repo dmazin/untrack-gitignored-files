71f56ea39a2c854e3afa8d096916338afc540b8f
Dmitry Mazin <dm@archvile.net>
2023-03-31 12:47:58 +0100

    Added functionality to fetch and generate .gitignore file for the specified language.

---
+++ core.py
@@ -0,0 +1,22 @@
+import sys
+import requests
+
+def fetch_gitignore(language):
+    base_url = "https://raw.githubusercontent.com/github/gitignore/master/"
+    file_name = f"{language}.gitignore"
+
+    response = requests.get(base_url + file_name)
+
+    if response.status_code == 200:
+        with open(".gitignore", "w") as gitignore_file:
+            gitignore_file.write(response.text)
+        print(f"Downloaded {language} .gitignore successfully.")
+    else:
+        print(f"Error: {language} .gitignore not found.")
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        language = sys.argv[1]
+        fetch_gitignore(language)
+    else:
+        print("Usage: python fetch-gitignore.py <language>")
