01ca6865738e98ad87d3c2a053780341746a9369
Dmitry Mazin <dm@archvile.net>
2023-03-31 15:38:07 +0100

    Update version and add gitpython to requirements.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fetch-gitignore",
-    version="0.3.0",
+    version="0.4.0",
     description="A command-line tool to download .gitignore files for specific languages from GitHub",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -17,6 +17,7 @@ setup(
     },
     install_requires=[
         "requests",
+        "gitpython",
     ],
     classifiers=[
         "Development Status :: 3 - Alpha",
