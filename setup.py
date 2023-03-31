56a41853c7e7cd7a060af312d2a451e746d4b725
Dmitry Mazin <dm@archvile.net>
2023-03-31 15:46:25 +0100

    Bumped version from 0.4.0 to 0.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="fetch-gitignore",
-    version="0.4.0",
+    version='0.4.1',
     description="A command-line tool to download .gitignore files for specific languages from GitHub",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
