6205f7d88c76e5bc25e571eb8f7a6d71c8736355
Dmitry Mazin <dm@archvile.net>
2023-04-04 22:03:36 +0100

    Bumped version from 0.5.0 to 0.6.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="untrack-gitignored-files",
-    version='0.5.0',
+    version='0.6.0',
     description="A command-line tool to list and untrack gitignored files.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
